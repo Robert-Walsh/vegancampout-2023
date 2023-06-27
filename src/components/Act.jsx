07c5d1f299c12907c5d64224a1652855d59dce67
Robert Walsh <robert.walsh1411@gmail.com>
2023-06-28 00:05:04 +0100

    remove spotify

--- src/components/Act.jsx
+++ src/components/Act.jsx
@@ -19,9 +19,6 @@ function Act({stage, name, time, day, mustSee, mightSee, onClick}){
     <div onClick={handleClick} style={{'cursor': 'pointer', 'backgroundColor': backgroundColor, 'marginLeft': '10px', 'borderRadius': '4px', 'border': 'solid 1px grey', 'boxShadow': '0 8px 6px -6px black', 'marginBottom': '10px', 'height':'auto'}}>
       <div style={{'fontWeight':'bold', 'padding': '3px'}}>
         {name.toUpperCase()}
-        <a href={`https://open.spotify.com/search/artist:${name}`} target="_blank" rel="noreferrer" style={{'position': 'absolute', 'right': '0', 'margin': '10px'}}>
-          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/84/Spotify_icon.svg/1982px-Spotify_icon.svg.png" style={{'width': '40px'}} alt="spotify"/>
-        </a>
       </div>
       <div style={{ 'padding': '3px'}}>
         {time}
--- src/components/PlanAct.jsx
+++ src/components/PlanAct.jsx
@@ -28,9 +28,6 @@ function PlanAct({stage, name, time, mustSee, mightSee, clash}){
       <div style={{'padding': '4px', 'marginLeft': '10px', 'marginTop': '0px'}}>
         <div style={{'fontWeight':'bold'}}>
           {name.toUpperCase()}
-          <a href={`https://open.spotify.com/search/artist:${name}`} target="_blank" rel="noreferrer" style={{'position': 'absolute', 'right': '0', 'margin': '15px'}}>
-            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/84/Spotify_icon.svg/1982px-Spotify_icon.svg.png" style={{'width': '40px'}} alt="spotify"/>
-          </a>
         </div>
         <div>
           {stage.toUpperCase()}
